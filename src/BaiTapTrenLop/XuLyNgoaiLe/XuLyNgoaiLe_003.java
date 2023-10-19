c8aee92e6f110d076a0318f139cf394e677a6b13
Vu Viet Duy <vuvietduy1234@gmail.com>
2023-10-19 10:25:06 +0700

    Oct 19, 2023

---
+++ src/BaiTapTrenLop/XuLyNgoaiLe/XuLyNgoaiLe_003.java
@@ -0,0 +1,15 @@
+package BaiTapTrenLop.XuLyNgoaiLe;
+
+public class XuLyNgoaiLe_003 {
+    void aMethod() throws Exception{
+        throw new Exception("Error");
+    }
+
+    public static void main(String[] args) {
+        try {
+
+        } catch (Exception e) {
+
+        }
+    }
+}
--- src/KhaiBaoLopVaDoiTuong/J04012_BaiToanTinhCong.java
+++ src/KhaiBaoLopVaDoiTuong/J04012_BaiToanTinhCong.java
@@ -1,8 +1,16 @@
 package KhaiBaoLopVaDoiTuong;
 
+import java.util.Scanner;
+
 public class J04012_BaiToanTinhCong {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner(System.in);
+        String n = sc.nextLine();
+        long s = sc.nextLong();
+        int w = sc.nextInt();
+        String p = sc.next();
+        Staff staff = new Staff(n, s,w,p);
+        System.out.println(staff);
     }
 }
 
@@ -20,16 +28,44 @@ class Staff {
         this.position = position;
     }
 
-    public Long getSalary () {
+    public long getSalary () {
         return workdays*basicSalary;
     }
 
-//    public Long reward () {
-//
-//    }
+    public long getReward () {
+        int phanTram = 0;
+        if (workdays >= 25) {
+            phanTram = 20;
+        } else if (workdays >= 20) {
+            phanTram = 10;
+        }
+        return getSalary() * phanTram / 100;
+    }
+
+    public long getAllowance () {
+        if (position.equals("GD")) {
+            return 250000;
+        } else if (position.equals("PGD")) {
+            return 200000;
+        } else if (position.equals("TP")) {
+            return 180000;
+        } else {
+            return 150000;
+        }
+    }
+
+    public long getIncome() {
+        return getAllowance() + getReward() + getSalary();
+    }
 
     @Override
     public String toString() {
-        return id + " " + name + " ";
+        return id + " " + name + " " + getSalary() + " " + getReward() + " " + getAllowance() + " " + getIncome();
     }
 }
+/*
+Bui Thi Trang
+45000
+23
+PGD
+ */
